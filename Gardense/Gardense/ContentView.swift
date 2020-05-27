ef0da11ee8ec8943cb25dfe8462168a06f8f62f8
ntceleste <nathanielceleste@gmail.com>
2020-05-27 12:59:43 -0500

    base swift template

---
+++ Gardense/Gardense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gardense
+//
+//  Created by Nate on 5/27/20.
+//  Copyright © 2020 Nate. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
